2360cb8ede2c732a081dee39e69a9e7872f8417e
unknown <vandyliu1999@gmail.com>
2021-04-11 22:19:39 -0700

    minimax

--- c4.pl
+++ c4.pl
@@ -1,7 +1,10 @@
 :- dynamic testNewBoard/1, testDrawFilledBoard/1.
-:- discontiguous computerMove/4.
+:- discontiguous computerMove/5.
 :- use_module(library(clpfd)).
 :- include(boards).
+
+% May need to give Prolog more RAM for the complicated algorithms.
+% set_prolog_flag(stack_limit, 8_147_483_648).
 
 %%%%%%%%%%%%%%%%%% Starting Game Logistics %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -10,13 +13,20 @@ tutorial :- write("Type the column followed by a period to place a marker. Eg. \
 playAgain :- nl,write("Type \"play.\" to play again!"),nl.
 
 chooseAlgo(FinalAlgo) :-
-    nl, write('Type \"simple.\" to play against a simple AI, \"random.\" to play against a random AI and anything else followed by a period to play agains Monte Carlo AI.'),
+    nl,
+    write('Type \"simple.\" to play against a simple AI,'),nl,
+    write('\"random.\" to play against a random AI,'),nl,
+    write('\"minimax.\" to play against a minimax AI,'),nl,
+    write('\"nd.\" to play against an AI that chooses moves along a normal distribution,'),nl,
+    write('and anything else followed by a period to play against Monte Carlo AI.'),
     read(Algo),
     getAlgo(Algo, FinalAlgo).
 
 getAlgo(Algo, simple) :- Algo = 'simple', write('You chose simple AI.').
 getAlgo(Algo, random) :- Algo = 'random', write('You chose random AI.').
-getAlgo(Algo, random) :- \+ Algo = 'simple', \+ Algo = 'random', write('You chose Monte Carlo AI.'). % Change later
+getAlgo(Algo, minimax) :- Algo = 'minimax', write('You chose minimax AI. Note: Positive score is better for red. Negative score is better for yellow.').
+getAlgo(Algo, normalDistribution) :- Algo = 'nd', write('You chose moves-normally-distributed AI.').
+getAlgo(Algo, minimax) :- \+ Algo = 'simple', \+ Algo = 'random', write('You chose Monte Carlo AI.'). % Change later
 
 chooseColour(FinalColour) :- 
     nl,write('Type \"red.\" to play as red or anything else followed by a period to play as yellow.'),nl,
@@ -35,10 +45,10 @@ testPlay :- testBoardAlmostFilled(Board), move(Board, player, red, simple).
 
 %%%%%%%%%%%%%%%%% Computer Algorithms %%%%%%%%%%%%%%%%%%%%%
 
-% computerMove(Board, AI, AvailableMoves, Move) is true if Move is the best move out of AvailableMoves given the Board and AI\
+% computerMove(Board, AI, AvailableMoves, Colour, Move) is true if Move is the best move out of AvailableMoves given the Board, AI and Colour of the computer.
 
 % Simple AI
-computerMove(_, simple, AvailableMoves, Move) :- getSimpleMove(AvailableMoves, Move, [4,5,3,6,2,7,1]). 
+computerMove(_, simple, AvailableMoves, _, Move) :- getSimpleMove(AvailableMoves, Move, [4,5,3,6,2,7,1]). 
 getSimpleMove([X], X, _).
 getSimpleMove(AvailableMoves, Column, [Column|_]) :- 
     member(Column, AvailableMoves).
@@ -46,8 +56,91 @@ getSimpleMove(AvailableMoves, Move, [Column|T]) :-
     notMember(Column, AvailableMoves), getSimpleMove(AvailableMoves, Move, T).
 
 % Random AI
-computerMove(_, random, AvailableMoves, Move) :- random_member(Move, AvailableMoves).
+computerMove(_, random, AvailableMoves, _, Move) :- random_member(Move, AvailableMoves).
 
+% Normal Distribution AI
+% Approximately: Mean of 4 with SD = 1.2
+% Probability Distribution: 0.332452 e^(-0.347222 (-4 + x)^2)
+% Cumulative Distribution: 0.5erfc(0.589256(4-x))
+% 7 appears 2% of time
+% 6 appears 9% of time
+% 5 appears 23% of time
+% 4 appears 32% of time
+% 3 appears 23% of time
+% 2 appears 9% of time
+% 1 appears 2% of time
+computerMove(_, normalDistribution, [Move], _, Move).
+computerMove(_, normalDistribution, AvailableMoves, _, Move) :-
+    length(AvailableMoves, L),
+    L > 1,
+    random_between(0, 100, X),
+    getMoveFromDistribution(X, MoveAttempt),
+    (notMember(MoveAttempt, AvailableMoves) -> 
+    computerMove(_, normalDistribution, AvailableMoves, _, Move);
+    Move is MoveAttempt).
+
+getMoveFromDistribution(X, Move) :-
+    ((X < 2) -> Move is 1 ;
+    (2 =< X, X < 11) -> Move is 2 ;
+    (11 =< X, X < 34) -> Move is 3 ;
+    (34 =< X, X < 66) -> Move is 4 ;
+    (66 =< X, X < 89) -> Move is 5 ;
+    (89 =< X, X < 98) -> Move is 6 ;
+    Move is 7).
+
+
+% Minimax AI with certain depth
+% Choose custom depth here:
+depth(5).
+
+maxPlayer(red).
+computerMove(Board, minimax, AvailableMoves, Colour, Move) :- 
+    depth(Depth),
+    playMoves(AvailableMoves, Depth, Colour, Board, ScoreMoves),
+    nl,write('Minimax (score, move):'),write(ScoreMoves),nl,
+    getBestMove(Colour, ScoreMoves, Move).
+
+playMoves([], _, _, _, []).
+playMoves([Col|RestCols], Depth, Colour, Board, [(Score, Col)|RestScoreMoves]) :-
+    playMove(Depth, Colour, Board, Col, Score), % Try each move in available columns
+    playMoves(RestCols, Depth, Colour, Board, RestScoreMoves).
+
+playMove(0, _, _, _, 0). % Reached max depth.
+playMove(Depth, Colour, Board, Move, Score) :-
+    \+ Depth = 0,
+    newBoard(Board, Move, Colour, NewBoard),
+    (win(NewBoard) -> 
+    (maxPlayer(Colour) ->
+        Score is (1 + Depth) ; % Quicker wins are worth more
+        Score is (-1 - Depth)) ;
+        getAvailableMoves(NewBoard, NewAvailableMoves),
+        (NewAvailableMoves == [] ->
+            Score is 0;
+            NewDepth is (Depth - 1),
+            otherColour(Colour, OtherColour),
+            playMoves(NewAvailableMoves, NewDepth, OtherColour, NewBoard, ScoreMoves),
+            getBestScore(ScoreMoves, OtherColour, Score)
+    )).
+
+getBestScore(ScoreMoves, Colour, Score) :-
+    sort(ScoreMoves, [(FirstScore, FirstMove)|RestScoreMoves]),
+    (maxPlayer(Colour) -> 
+        last([(FirstScore, FirstMove)|RestScoreMoves], (LastScore, _)),
+        Score = LastScore ;
+        Score = FirstScore
+    ).
+
+getBestMove(Colour, ScoreMoves, Move) :-
+    getBestScore(ScoreMoves, Colour, Score),
+    possibleMovesToMake(Score, ScoreMoves, PossibleMoves),
+    computerMove([], normalDistribution, PossibleMoves, Colour, Move). 
+
+
+possibleMovesToMake(_, [], []).
+possibleMovesToMake(Score, [(Score,Move)|RestScoreMoves], [Move|RestMoves]) :- 
+    possibleMovesToMake(Score, RestScoreMoves, RestMoves).
+possibleMovesToMake(Score, [(OtherScore,_)|RestScoreMoves], RestMoves) :- 
+    \+ Score = OtherScore, possibleMovesToMake(Score, RestScoreMoves, RestMoves).
 
 %%%%%%%%%%%%%%%%%%%%%%% Gameplay Logistics %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -61,7 +154,7 @@ move(Board, player, Colour, Algo) :-
 
 move(Board, computer, Colour, Algo) :-
     getAvailableMoves(Board, AvailableMoves),
-    computerMove(Board, Algo, AvailableMoves, CPUMove),
+    computerMove(Board, Algo, AvailableMoves, Colour, CPUMove),
     newBoard(Board, CPUMove, Colour, NewBoard),
     transitionMove(NewBoard, computer, Colour, Algo).
 
